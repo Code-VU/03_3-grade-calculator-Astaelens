1611ec106689e369601f951b124d716cac82024f
Alex Staelens <Alex.Staelens@vu.com>
2023-01-29 14:52:37 -0600

    cleaned up if statements

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,22 +1,26 @@
 def calculateGrade():
     # Implement your solution in between the two comment blocks
-    print("Calculating Grade")
+    # print("Calculating Grade")
     # This first line is provided for you
 
-    hrs = float(input("Enter score:"))
-    if hrs<=1 & hrs>=.9:
-        print("A")
-    elif hrs >=.8:
-        print("B")
-    elif hrs>=.7:
-        print("C")
-    elif hrs>=.6:
-        print("D")
-    elif hrs>=0 & hrs<.6:
-        print("F")
-    else:
+    try:
+        hrs = float(input("Enter score: "))
+        if hrs>1:
+            print("Bad score") 
+        elif hrs>=.9:
+            print("A")
+        elif hrs >=.8:
+            print("B")
+        elif hrs>=.7:
+            print("C")
+        elif hrs>=.6:
+            print("D")
+        elif hrs>=0 and hrs<.6:
+            print("F")
+        else:
+            print("Bad score")
+    except:
         print("Bad score")
-
     # end assignment
 
 ## If you want to test locally before you try to sync
